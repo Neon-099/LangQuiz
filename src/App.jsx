c64189677f54672e1ed85f749a508e9c3708b876
Neon-099 <Emmannatividad099@gmail.com>
2025-07-30 22:33:00 +0800

    ft: refactor App component to use Landing page and clean up routing structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Landing from './pages/LandingPage.jsx';
 import Home from './pages/Home.jsx';
 import HtmlQuiz from './pages/HtmlQuiz.jsx';
 
@@ -10,12 +11,11 @@ function App() {
   return (
 
     <Router>
-      <div className="max-w-xl mx-auto p-4">
-        <Routes>
-          <Route path="/" element={<Home />} /> {/*(/) to declare that this is the start point*/}
-          <Route path='/quiz/html' element={<HtmlQuiz />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/quiz/home" element={<Home />} /> {/*(/) to declare that this is the start point*/}
+        <Route path='/quiz/html' element={<HtmlQuiz />} />
+      </Routes>
     </Router>
   )
 }
