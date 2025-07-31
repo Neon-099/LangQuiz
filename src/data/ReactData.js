25485462c1e59e20f9e8556ba6d2cc762c91f9db
Neon-099 <Emmannatividad099@gmail.com>
2025-07-31 20:23:50 +0800

    ft: add reactQuestion data and enhance useQuiz for scoring and review functionality

---
+++ src/data/ReactData.js
@@ -0,0 +1,26 @@
+export const reactQuestion = [
+    {
+    id: 0,
+    question: "What is React.js primarily used for?",
+    options: [
+      "Database management",
+      "Building user interfaces",
+      "Server-side programming",
+      "Mobile app development only"
+    ],
+    difficulty: 'easy',
+    answer: 1
+  },
+  {
+    id: 1,
+    question: "Which command creates a new React app?",
+    options: [
+      "npm init react-app",
+      "npm create-react-app",
+      "npx create-react-app",
+      "react new app"
+    ],
+    difficulty: 'easy',
+    answer: 2
+  }
+]
--- src/hooks/useQuiz.js
+++ src/hooks/useQuiz.js
@@ -1,10 +1,19 @@
 import {useState, useEffect} from 'react';
 import { useShuffleQuiz } from './useShuffledQuiz';
-
+import {useScoreStore } from '../stores/scoreStore.js';
+import {calculateScoring} from '../utils/calculateScoring.js'
 export const useQuiz = ( questions ) => {
+    
+  //DESTRUCTURING COMP
+    const {
+      currentScore,
+      increaseScoreBy,
+      updateHighScore,
+      resetScore
+    } = useScoreStore();
+
     const [timeCount, setTimeCount] = useState(10);
     const [index, setIndex] = useState(0);
-    const [score, setScore] = useState(0);
     const [start, setStart] = useState(false);
     const [finished, setFinished] = useState(false);
     const [question, setQuestion] = useState([]); //store here the quiz sections
@@ -19,11 +28,12 @@ export const useQuiz = ( questions ) => {
       medium: 10,
       hard: 5,
     };
-    
+
     //AUTO START WHEN PAGE LOAD (only run once(w/o deps))
     useEffect(() => {
       setQuestion(questions);
       setStart(true);
+      currentScore;
     }, []); //declare deps to run only once without breaking the effect behavior (need run only once)
     
     //INITIALIZE TIMER WHEN QUESTIONS ARE SHUFFLED
@@ -51,46 +61,60 @@ export const useQuiz = ( questions ) => {
     useEffect(() => {
       if(timeCount === 0 && start && !finished) {
         handleTimesUp();
+        updateHighScore()
       }
     }, [timeCount]); //to updates the time count every call
     
     
-    //HANDLE NEXT QUESTION
+    //HANDLE NEXT QUESTION              
     const handleNextQuestion = (choice) => {
+    
+      const current = shuffledQuestions[index]; //get current index (instead of this well pass it in a PARAMS)
+      const correctIndex = current.answer; 
+      const isCorrect = choice === correctIndex ? true : false;
+      
+      const selectedText = current.options[choice]; //get selected STR
+      const correctText = current.options[correctIndex]   //get correct STR
+
+      //SET ANSWERS HERE (REVIEW MODE)
+      setAnswers((prev) => [
+          ...prev,
+          {   
+            question: shuffledQuestions[index].question, 
+            choices: selectedText,
+            CorrectAnswer: correctText,
+            isCorrect,
+          }
+      ]);
+   const difficulty = current.difficulty;
       //SCORING LOGIC
       const correct = shuffledQuestions[index].answer; //1.1 use shuffle and find index also the answer
-    if(choice === correct) { //if true the score will increase to 1
-        setScore((prev) => prev + 1); //state usage when want to increase (1.2)
+      if(choice === correct && difficulty) { //if true the score will increase to 1
         
-        setAnswers((prev) => [
-            ...prev,
-            {
-                question: shuffledQuestions[index].question,
-                choice: false,
-                CorrectAnswer: shuffledQuestions[index].answer,
-                isCorrect: true,
-            }
-        ]);
-    }
-      
-    const nextIndex = index + 1; //increment the index each call so that it will be the basis if which question current in
-    if(nextIndex < question.length) {
-        setIndex(nextIndex);
-    
-        const nextQuestion = shuffledQuestions[nextIndex]; //GET THE QUESTION BASED ON THE INDEX 
-        const newTime = baseTime[nextQuestion.difficulty] || 10; //like act (COMPARISON TO KNOW WHAT KIND OF TIME TO USE THAT WILL BASED ON DIFFICULTY) 
-        setTimeCount(newTime); //UPDATE THE TIMER BASED ON THE DIFFICULTY
-      
-        
-    } else {
-        setFinished(true);
-        setTimeCount(0)
+        //DIFFICULTY BASED POINTS
+     
+        const points = calculateScoring(difficulty);
+        increaseScoreBy(points)
       }
-    }
+
+      const nextIndex = index + 1; //increment the index each call so that it will be the basis if which question current in
+      if(nextIndex < question.length) {
+          setIndex(nextIndex);
+
+          const nextQuestion = shuffledQuestions[nextIndex]; //GET THE QUESTION BASED ON THE INDEX 
+          const newTime = baseTime[nextQuestion.difficulty] || 10; //like act (COMPARISON TO KNOW WHAT KIND OF TIME TO USE THAT WILL BASED ON DIFFICULTY) 
+          setTimeCount(newTime); //UPDATE THE TIMER BASED ON THE DIFFICULTY
+          
+      } else {
+          setFinished(true);
+          setTimeCount(0)
+        }
+      }
     
     //HANDLE WHEN TIME IS UP
     const handleTimesUp = () => {
       setFinished(true);
+      updateHighScore();
     }
     
     const handleRestart = () => {
@@ -98,16 +122,18 @@ export const useQuiz = ( questions ) => {
     const time = baseTime[firstQuestion.difficulty] || 10; //the resetted index count is 0
 
     setIndex(0);
-    setScore(0);
     setFinished(false);
     setStart(true);
     setTimeCount(time); //more likely this will be in reset / 0
     setAnswers([]);
+    setSeeReview(false);
+    resetScore();
     };
 
     const handleQuit = () => {
     setFinished(true);
     setStart(false);
+    updateHighScore()
     };
 
     const handleReviewQuestion = () => {
@@ -117,7 +143,7 @@ export const useQuiz = ( questions ) => {
     return {
         //RETURN THE STATE SO THAT WE CAN USE THEM 
         index,
-        score,
+        currentScore,
         start,
         finished,
         timeCount,
